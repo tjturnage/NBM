03569fb45389ef1537f04ad7a1e863bc2d1401d2
T.J <thomas.turnage@noaa.gov>
2019-11-11 23:26:07 -0500

    Add files via upload

--- NBM_partners.py
+++ NBM_partners.py
@@ -15,36 +15,6 @@ https://para.nomads.ncep.noaa.gov/pub/data/nccf/com/blend/para/blend.20191105/07
 import os
 import sys
 
-def dtList_nbm(run_dt,bulletin_type,tz_shift):
-    """
-      Create pandas date range of forecast valid times based on bulletin
-      issuance time and bulletin type.
-    
-      Parameters
-      ----------
-        run_dt : python datetime object
-                 Contains yr,mon,date,hour associated with bulletin issue time
-
-  bulletin_type: string
-
-                 'nbhtx' -- hourly guidance ( hourly)
-
-                 This is required to define model forecast hour start and 
-                 end times as a well as forecast hour interval.
-                  
-      Returns
-      -------
-           pandas date/time range to be used as index as well as start/end times
-    """
-
-    fcst_hour_zero_utc = run_dt + timedelta(hours=0)
-    fcst_hour_zero_local = fcst_hour_zero_utc - timedelta(hours=tz_shift)
-    #pTime = pd.Timestamp(fcst_hour_zero_utc)
-    pTime = pd.Timestamp(fcst_hour_zero_local)
-    idx = pd.date_range(pTime, periods=27, freq='H')
-
-    return idx, fcst_hour_zero_local
-
 
 def round_values(x,places,direction):
     amount = 10**places
@@ -53,72 +23,12 @@ def round_values(x,places,direction):
     if direction == 'down':
         return int(math.floor(x / float(amount))) * int(amount)
 
-def wind_chill(t,s):
-    """
-    Returns wind chill in degress F
-    Inputs:
-        t   : temperatures in degrees F
-        s   : wspd in MPH
-    """    
-    wc = 0.6215*t + 35.74 - 35.75*(s**0.16) + (0.4275*t)*(s**0.16)
-
-
-    if wc >= -15:
-        fbt = 4
-
-    if wc < -15:
-        fbt = 3
-
-    if wc < -35:
-        if wspd > 20:
-            fbt = 2
-        else:
-            fbt = 3
-
-    if wc < -45:
-        if wspd > 10:
-            fbt = 2
-        else:
-            fbt = 3
-                
-    return round(wc), fbt
-
-
-def categorize(data_list,element):
-    """dat
- [0,1,2,3,4,5,6],'major_yticks_labels':['0.00','0.01','0.05','0.10','0.2','0.3','0.5'], 
-        zr_list   : one hourly snow in hundredths
-    
-    """
-
-    vis_dict = {'0.0':0,'0.3':1,'0.6':2,'1':3,'2':4,'4':3,'6':6}
-    zr_dict = {'-0.1':0,'1':1,'8':2,'18':3,'28':4,'45':5}
-    sn_dict = {'-0.1':0,'0.05':1,'0.15':2,'0.45':3,'0.75':4,'0.95':5,'1.35':6,'1.85':7}  
-    #sn_dict = {'-0.1':0,'0.05':1,'0.15':2,'0.33':3,'0.75':4,'1.5':5,'2.5':6}    
-
-    if element == 'sn':
-        data_dict = sn_dict
-    if element == 'zr':
-        data_dict = zr_dict    
-    if element == 'vis':
-        data_dict = vis_dict    
-
-    category_list = []  
-    for x in range(0,len(data_list)):
-        val = data_list[x]
-        for key in data_dict:
-            if val > float(key):
-                x_cat = data_dict[key]
-            
-        category_list.append(x_cat)
-
-    return category_list
 
 def temperature_bounds(t_list,wc_list):
     max_val = np.max(t_shifted_list)
     min_val = np.min(wc_list)
     high_list = np.arange(100,0,-10)
-    low_list = np.arange(0,100,10)
+
     for h in range(0,(len(high_list))):
         if high_list[h] > max_val:
             upper_limit = high_list[h]
@@ -126,12 +36,12 @@ def temperature_bounds(t_list,wc_list):
             break
     upper_limit = upper_limit + 10     
 
+    low_list = np.arange(0,100,10)
     for l in range(0,(len(low_list))):
         if low_list[l] < min_val:
             lower_limit = low_list[h]
         else:
             break
-
     lower_limit = lower_limit - 20  
     
     tick_list = np.arange(lower_limit,upper_limit,10)
@@ -140,7 +50,6 @@ def temperature_bounds(t_list,wc_list):
         tick_label = str(int(tick_list[t] - 30))
         tick_labels.append(tick_label)
     print(tick_list,tick_labels)
-    
     return tick_list,tick_labels    
 
 def download_nbm_bulletin(url,fname,path_check):
@@ -150,7 +59,20 @@ def download_nbm_bulletin(url,fname,path_check):
         print('downloading ... ' + str(url))
         open(dst, 'wb').write(r.content)
     return dst
-        
+
+"""
+base_dir = 'C:/data/'        
+ymd = '20191111'
+for hr in range(0,23):
+    hour = str(hr)
+    if len(hour) < 2:
+        hour = '0' + hour
+    print(hour)
+    url = 'https://para.nomads.ncep.noaa.gov/pub/data/nccf/com/blend/para/blend.' + ymd + '/' + hour + '/text/blend_nbhtx.t' + hour + 'z'
+    fname = 'ymd' + '_' + hour
+    fpath = os.path.join(base_dir,fname)
+    download_nbm_bulletin(url,fname,'hi')
+"""
     
 def u_v_components(wdir, wspd):
     # since the convention is "direction from"
@@ -192,14 +114,15 @@ import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from my_functions import GridShader
+from my_functions import GridShader,wind_chill, time_to_frostbite, dtList_nbm, categorize
 from matplotlib.dates import DateFormatter
 import matplotlib.dates as mdates
 import requests
 from datetime import datetime, timedelta
-import matplotlib.transforms
 
-download = True
+import matplotlib.transforms
+from reference_data import nbm_station_dict
+download = False
 bulletin_type = 'nbhtx'
 
 now = datetime.utcnow()
@@ -209,75 +132,34 @@ hour = now2.strftime('%H')
 #url = 'https://para.nomads.ncep.noaa.gov/pub/data/nccf/com/blend/para/blend.20191107/15/text/blend_nbhtx.t15z'
 url = 'https://para.nomads.ncep.noaa.gov/pub/data/nccf/com/blend/para/blend.' + ymd + '/' + hour + '/text/blend_' + bulletin_type + '.t' + hour + 'z'
 
-
-state2timezone = { 'AK': 'US/Alaska', 'AL': 'US/Central', 'AR': 'US/Central', 'AS': 'US/Samoa', 'AZ': 'US/Mountain', 'CA': 'US/Pacific', 'CO': 'US/Mountain', 'CT': 'US/Eastern', 'DC': 'US/Eastern', 'DE': 'US/Eastern', 'FL': 'US/Eastern', 'GA': 'US/Eastern', 'GU': 'Pacific/Guam', 'HI': 'US/Hawaii', 'IA': 'US/Central', 'ID': 'US/Mountain', 'IL': 'US/Central', 'IN': 'US/Eastern', 'KS': 'US/Central', 'KY': 'US/Eastern', 'LA': 'US/Central', 'MA': 'US/Eastern', 'MD': 'US/Eastern', 'ME': 'US/Eastern', 'MI': 'US/Eastern', 'MN': 'US/Central', 'MO': 'US/Central', 'MP': 'Pacific/Guam', 'MS': 'US/Central', 'MT': 'US/Mountain', 'NC': 'US/Eastern', 'ND': 'US/Central', 'NE': 'US/Central', 'NH': 'US/Eastern', 'NJ': 'US/Eastern', 'NM': 'US/Mountain', 'NV': 'US/Pacific', 'NY': 'US/Eastern', 'OH': 'US/Eastern', 'OK': 'US/Central', 'OR': 'US/Pacific', 'PA': 'US/Eastern', 'PR': 'America/Puerto_Rico', 'RI': 'US/Eastern', 'SC': 'US/Eastern', 'SD': 'US/Central', 'TN': 'US/Central', 'TX': 'US/Central', 'UT': 'US/Mountain', 'VA': 'US/Eastern', 'VI': 'America/Virgin', 'VT': 'US/Eastern', 'WA': 'US/Pacific', 'WI': 'US/Central', 'WV': 'US/Eastern', 'WY': 'US/Mountain', '' : 'US/Pacific', '--': 'US/Pacific' }
-time_shift_dict = {'US/Eastern':4,'US/Central':5,'US/Mountain':6,'US/Pacific':7,'US/Hawaii':7,'US/Alaska':8,'Pacific/Guam':9,'America/Puerto_Rico':3,'America/Virgin':3}
-
-    
 fin = 'C:/data/scripts/NBM/NBM_stations.txt'
 fout = 'C:/data/scripts/NBM/NBM_MI_stations.txt'
 
-station_master = {}
-with open(fin,'r') as src:
-    with open(fout,'w') as dst:
-        for line in src:
-            elements = line.split(',')
-            station_id = str(elements[0])
-            station_name = str(elements[1])
-            state = str(elements[2])
-            if state in state2timezone.keys():
-                lat = float(elements[3])
-                lon = float(elements[4])
-                utc_shift = time_shift_dict[state2timezone[state]]
-                #print(station,state,lat,lon,time_shift_dict[utc_shift])
-                #station_info[station] = ([('state', state) , ('utc_shift', time_shift_dict[utc_shift]) ,('lat', lat) , ('lon' , 20)] )
-                #station_master[station] = ([('state',state),('time_shift',utc_shift),('lat',lat),('lon',lon)])
-                station_master[station_id] = ({'name':station_name,'state':state,'time_shift':utc_shift,'lat':lat,'lon':lon})
+station_master = nbm_station_dict()
 
-            else:
-                utc_shift = 0
-
-station_mini = {}
-lats = []
-lons = []
-names = []
-plt.figure(figsize=(16, 16))
+mi_stations = []
 for key in station_master:
-    station_id = key
-    station_description = station_master[key]['name']
-    lat = station_master[key]['lat']
-    lon = station_master[key]['lon']
     if station_master[key]['state'] == 'MI':
-        #if station_master[key]['lat'] > 44.5 and station_master[key]['lon'] > -88:
-
-        lats.append(lat)
-        lons.append(lon)
-        names.append(key)        
-        station_mini[key] = station_master[key]
-        lats_tuple = tuple(lats)    
-        lons_tuple = tuple(lons)
-        if lat < 44.5 and lon > -86.7 and lon < -84.3:
-            plt.plot(station_master[key]['lon'],station_master[key]['lat'],'ro')
-            plt.text(lon+.03, lat+.03, key, fontsize=10)
-        else:
-            pass
-plt.xlim(-86.7,-84.3)
-plt.ylim(41.5,44.5)
-#plt.yticks(np.linspace(0,250,6,endpoint=True))
-#plt.xticks(np.linspace(0,6,7,endpoint=True))
-
+        mi_stations.append(key)
 #products = ['t_bar','wc_bar','time_fb_bar','wind','vis_bar','s01_bar']
 products = ['t_bar','time_fb_bar','wind','vis_test','sn_test','zr_test']
 fname = 'nbm_raw_hourly.txt'
 #fname = 'C:/data/scripts/NBM/20191110/blend_nbhtx.t18z'
 #C:\data\scripts\NBM\20191109
-for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
-                     'KMEAR','KRQB','KMOP','KMKG','KAMN','KBIV','KGRR',
-                     'KY70','KLAN','KFPK','KTEW','KLWA','KPAWP','KAZO','KBTL','KRMY',
-                     'KJXN','KBEH'):
+map_plot_stations = {}
+#for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
+#                     'KMEAR','KRQB','KMOP','KMKG','KAMN','KBIV','KGRR',
+#                     'KY70','KLAN','KFPK','KTEW','KLWA','KPAWP','KAZO','KBTL','KRMY',
+#                     'KJXN','KBEH'):
+#for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
+#                     'KMEAR','KRQB','KMOP',
+#                     'KY70','KLAN',):
+for key in ['KGRR']:
     #if s in ['KAZO','KGRR','KMKG','KMOP','KMKG','KBIV']:
     station_id = key
     station_description = station_master[key]['name']
+    lat = station_master[key]['lat']
+    lon = station_master[key]['lon']
     column_list = []
     station_found = False
     utc_shift = station_master[key]['time_shift']
@@ -319,11 +201,9 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
                 dst.close()
                 break 
 
-    
     nbm_old = None
     nbm = None
     
-
     nbm_old = pd.read_fwf(trimmed_nbm_file, widths=(5,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3))
     elements = column_list[1:]
 
@@ -358,8 +238,6 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
     # Either can be done independently, but I usually do both to have the later 
     # option of plotting either way.
     
-    # TMP and DPT will go on same panel, so using min(DPT) and max(TMP) to define bounds
-
     t_list = nbm.TMP.tolist()
     t_list = np.asarray(t_list, dtype=np.float32)
     t_shifted_list = t_list + 30
@@ -380,12 +258,17 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
     wc_list = []
     time_to_fb_list = []
     for chill in range(0,len(wspd_list)):
-        wc,time_to_fb = wind_chill(t_list[chill],wspd_list[chill])
+        wc = wind_chill(t_list[chill],wspd_list[chill])
+        time_to_fb = time_to_frostbite(wc,wspd_list[chill])
         wc_list.append(wc)
         time_to_fb_list.append(time_to_fb)
     wc_list = np.asarray(wc_list, dtype=np.float32)
+    wc_cat = categorize(wc_list,'wc')
     wc_list = wc_list + 30
-
+    map_plot_stations[key] = {'lon':lon,'lat':lon,'wc_cat':wc_cat[0]}
+    # Temp (t) and wind chill (wc) go on same panel, 
+    # so using min(wc) and max(t) to define bounds for 'twc'
+    # using a temperature_bounds function
     twc_tick_list,twc_tick_labels = temperature_bounds(t_shifted_list,wc_list)
     
     # sometimes we have to convert units because they're in tenths or hundredths
@@ -394,17 +277,26 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
     vis.clip(upper=7.0,inplace=True)
     vis_list = nbm.VIS.tolist()
     
-
     p_ra_list = nbm.PRA.tolist()
     p_sn_list = nbm.PSN.tolist()
     p_zr_list = nbm.PZR.tolist()
     p_pl_list = nbm.PPL.tolist()
 
-
     # hourly snow amount, convert integers to tenths of an inch
     nbm.S01 = nbm.S01.multiply(0.1)
     s01_list = nbm.S01.tolist()
+    
+    sn_plot = []
+    ts_list = []
     sn_list = nbm.S01.tolist()
+    sn_cat_list = categorize(sn_list,'sn')
+    for t in (np.arange(0,12,2)):
+        ts = nbm.index[t]
+        t_str = ts.strftime('%d %h %Y %H')
+        t_str = ts.strftime('%B %d, %Y - %I %p')
+        sn_plot.append(sn_cat_list[t])
+        ts_list.append(t_str)
+    map_plot_stations[key] = {'snow':sn_plot, 'time_string':ts_list, 'lon':lon,'lat':lat}
     sn_cat_list = categorize(sn_list,'sn')
 
     i01_list = nbm.I01.tolist()
@@ -475,10 +367,7 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
          'ymin':0.0,'ymax':1.01,'yticks':[0,0.25,0.5,0.75,1],
          'ytick_labels':['0','1/4','1/2','3/4','1'], 'title':'Snow\nAccum' }
 
-
-
     prods['wind'] = {'data': wspd, 'color':(0.5, 0.5, 0.5, 0.8),'title':'Wind\nSpeed\n& Gust'}
-
 
     prods['t_bar'] = {'data': t_shifted_list, 'color':(0.9, 0.1, 0.1, 0.7),
          'ymin':30,'ymax':80,'bottom':0,
@@ -500,10 +389,6 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
          'ymin':-5,'ymax':105,'yticks':[0,25,50,75,100],
          'ytick_labels':['0','25','50','75','100'], 'title':'Sky cover\n(%)'}
 
-#    prods['vis_bar'] = {'data': vis_list, 'color':(0.7, 0.7, 0.3, 1),
-#         'ymin':-0.5,'ymax':8,'yticks':[1, 3, 5, 7],
-#         'ytick_labels':['1','3','5', '>6'], 'title':'Visibility\n(miles)'}
-
     prods['sn_01_bar'] = {'data': sn_01_list, 'color':(0.3, 0.3, 0.8, 0.8),
          'ymin':p_min,'ymax':p_max,'yticks':prob_yticks,'ytick_labels':prob_ytick_labels,
          'title':'Abs Prob\nSnow(%)'}
@@ -512,6 +397,7 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
          'ymin':0,'ymax':7,'bottom':0,
          'major_yticks':[0,1,2,3,4,5,6,7],'major_yticks_labels':['0.0','0.1','0.2','0.5','0.8','1.0','1.5','2.0'],    
          'title':'Snow\nAccum\n(in)' }
+
     sn_dict = {'-0.1':0,'0.05':1,'0.15':2,'0.45':3,'0.75':4,'0.95':5,'1.45':6}        
 
     prods['zr_test'] = {'data': zr_list, 'color':(204/255,153/255,204/255, 0.7),
@@ -561,19 +447,9 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
         a.xaxis.set_major_locator(hours)
         a.xaxis.set_major_formatter(myFmt)
 
-
         plt.setp( a.xaxis.get_majorticklabels(), ha="left",rotation=0 )
         a.yaxis.set_label_coords(-0.08,0.25)
           
-        if y == 't':
-            a.grid()
-            a.get_xaxis().set_visible(False)
-            gs = GridShader(a, facecolor="lightgrey", first=first_gray, alpha=0.5)            
-            a.set_ylim(prods[y]['ymin'],prods[y]['ymax'])
-            a.set(yticks = prods[y]['yticks'], yticklabels = prods[y]['ytick_labels'])
-            a.set_ylabel(prods[y]['title'], rotation=0)
-            a.plot(prods['t']['data'],linewidth=2,color=prods['t']['color'])
-
         if y == 'ra_01':
             a.grid()
             a.get_xaxis().set_visible(False)
@@ -605,16 +481,6 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
                 a.text(p, 0.28, f'{s}',horizontalalignment='center',color=[0,0,1,0.9])
                 a.text(p, 0.08, f'{g}',horizontalalignment='center',color=[0.6,0,1,0.6])
 
-
-
-        # these are dataframe slices that use matplotlib plot to create time series
-        if y in ['p01','p06','p_zr','p_pl','sky','vis']:
-            gs = GridShader(a, facecolor="lightgrey", first=first_gray, alpha=0.5) 
-            a.set_ylim(prods[y]['ymin'],prods[y]['ymax'])
-            a.set(yticks = prods[y]['yticks'])
-            a.plot(prods[y]['data'],color=prods[y]['color'])
-            a.set_ylabel(prods[y]['title'], rotation=0)
-            a.set(yticks = prods[y]['yticks'], yticklabels = prods[y]['ytick_labels'])
 
         # specialized treatment for ranges and gridlines
         if y in ['sn_test','zr_test','vis_test']:
@@ -668,6 +534,4 @@ for key in ('KMBL','KCAD','KHTL','KLDM','BDWM4','EVAM4',
     #plt.show()
     plt.savefig(image_dst_path,format='png')
     plt.close()
-
-
 
